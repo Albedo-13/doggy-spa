39fa25b652a5952d55430a45984624696b836bf3
Pavel Prakapenia <prokopenya.personal@gmail.com>
2026-01-20 19:02:18 +0300

    test: another #6 attempt to fix playwright workflow

---
+++ playwright/e2e/navigation.spec.ts
@@ -0,0 +1,38 @@
+import { expect, test } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/');
+  await page.waitForLoadState('networkidle');
+});
+
+test('Should redirect to all available pages', async ({ page }) => {
+  await page.getByRole('link', { name: 'info' }).first().click();
+  await page.waitForLoadState('networkidle');
+  await expect(page).toHaveURL('/info');
+
+  await page.getByRole('link', { name: 'Spa Services' }).first().click();
+  await page.waitForLoadState('networkidle');
+  await expect(page).toHaveURL('/spa-services');
+
+  await page.getByRole('link', { name: 'Book Appointment' }).first().click();
+  await page.waitForLoadState('networkidle');
+  await expect(page).toHaveURL('/book-appointment');
+
+  await page.waitForLoadState('networkidle');
+
+  await page.getByRole('link', { name: 'Blog' }).first().click();
+  await page.waitForLoadState('networkidle');
+  await expect(page).toHaveURL('/blog');
+
+  await page.getByRole('link', { name: 'About Us' }).first().click();
+  await page.waitForLoadState('networkidle');
+  await expect(page).toHaveURL('/about-us');
+
+  await page.getByRole('link', { name: 'Contact Us' }).first().click();
+  await page.waitForLoadState('networkidle');
+  await expect(page).toHaveURL('/contact-us');
+
+  await page.getByRole('link', { name: 'Home' }).first().click();
+  await page.waitForLoadState('networkidle');
+  await expect(page).toHaveURL('/');
+});
